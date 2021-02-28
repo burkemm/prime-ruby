81e3ccdb11f1af2c433b7d5ed4d37457fa4851aa
burkemm <burkemm84@gmail.com>
2021-02-27 22:23:29 -0600

    commit tree 1485a89e40df23fa724f80f3deddb0a3031e14a9

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    elsif
+        # check all the numbers between 2 and the number minus 1 and send them to an array and loop through all of them to check to see if they are possible factors
+        # use the modulus to see if the remainder equals 0
+        (2..number-1).to_a.all? do | factor |
+        return number % factor != 0
+    else
+         return true
+    end
+
+    end
+end
+
